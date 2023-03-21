2ad7b9d50627e8953d8d5f28c9b9cb22448aa952
ashria022 <ashria045@gmail.com>
2023-03-21 11:51:04 +0600

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import Card from './Components/Card'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Card />
+<Footer />
+    </div>
+  );
+}
+
+export default App;
